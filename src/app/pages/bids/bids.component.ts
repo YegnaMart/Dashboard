b6fd6fc5e59aaa7a7b402eb759f42b632718c999
Yeab13 <yeab13me@gmail.com>
2020-12-13 18:12:50 +0300

    Bid List Done Perfectly

--- src/app/pages/bids/bids.component.ts
+++ src/app/pages/bids/bids.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs/tab-group';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-bids',
@@ -8,7 +10,59 @@ import { Component, OnInit } from '@angular/core';
 export class BidsComponent implements OnInit {
 
   public isLoading : Boolean = true;
-  constructor() { }
+  public availableBids = []
+  public availabledClosedBids = []
+  public availabledScheduledBids = []
+  public availableAllBids = []
+
+  public activeBid = false;
+
+  constructor(private httpService : HttpService){
+    
+    this.httpService.getBids().subscribe((resp:any)=> {
+
+      this.availableBids = resp.filter(_bid => {
+        if(_bid.status === "inactive"){
+          return true
+        }
+        return false
+      })
+
+      this.availabledClosedBids = resp.filter(_bid => {
+        if(_bid.status === "closed"){
+          return true
+        }
+        return false
+      })
+
+      this.availabledScheduledBids = resp.filter(_bid => {
+        if(_bid.status === "scheduled"){
+          return true
+        }
+        return false
+      })
+      
+      if(this.availableBids.length > 0){
+        this.availableAllBids = [...this.availableBids]
+      }
+
+      if(this.availabledClosedBids.length > 0){
+        this.availableAllBids = [...this.availabledClosedBids]
+      }
+
+      if(this.availabledScheduledBids.length > 0){
+        this.availableAllBids = [...this.availabledScheduledBids]
+      }
+      
+      if(this.availableAllBids.length > 0){
+        this.activeBid = this.availableAllBids[0]
+      }
+      console.log(this.availableAllBids)
+      setTimeout(()=>{
+        this.isLoading = false;
+      },3000)
+    })
+  }
 
   ngOnInit(): void {
 
@@ -19,8 +73,76 @@ export class BidsComponent implements OnInit {
 
   postbid(){
 
-    
+    //bidNo , biddingFee , product (list of product ... product id ..>>> ) , initialBiddingPrice , startingDate , closingDate , postedBy , postedBy ( warehouse object id)
 
+  }
+
+  getImage(imagePath){
+    
+    return `${this.httpService.getHost()}/${imagePath.replace('\\','//')}`
+  }
+
+  tabChanged(tabChangeEvent: MatTabChangeEvent): void {
+    console.log('tabChangeEvent => ', tabChangeEvent);
+    console.log('index => ', tabChangeEvent.index);
+
+    if(tabChangeEvent.index === 0){ //All
+
+      if(this.availableAllBids.length > 0){
+        this.activeBid = this.availableAllBids[0]
+      }else {
+        this.activeBid = false
+      }
+      
+    }else if(tabChangeEvent.index === 1) { //Ongoing
+
+      if(this.availableBids.length > 0){
+        this.activeBid = this.availableBids[0]
+      }else {
+        this.activeBid = false
+      }
+
+    }else if(tabChangeEvent.index === 2){ //Closed
+      
+      if(this.availabledClosedBids.length > 0){
+        this.activeBid = this.availabledClosedBids[0]
+      }else {
+        this.activeBid = false
+      }
+
+    }else if(tabChangeEvent.index === 3){ //Scheduled
+
+      if(this.availabledScheduledBids.length > 0){
+        this.activeBid = this.availabledScheduledBids[0]
+      }else {
+        this.activeBid = false
+      }
+    }else {
+
+      this.activeBid = false
+    }
+
+  }
+
+  
+  formatCurrency(price){
+
+    var formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'ETB',
+    
+      // These options are needed to round to whole numbers if that's what you want.
+      //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+      //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    });
+
+    return formatter.format(price); 
+
+  }
+
+  showPreview(bid){
+    
+    this.activeBid = bid;
   }
 
 }
